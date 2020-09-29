1d44c94da69fec13c1f5de2d15853c0609d0163f
Valerie Robb <valthom@microsoft.com>
2020-09-28 17:06:27 -0700

    Copyright info to cs source files

--- src/Microsoft.AzureIntegrationMigration.Runner/Model/IApplicationModelProvider.cs
+++ src/Microsoft.AzureIntegrationMigration.Runner/Model/IApplicationModelProvider.cs
@@ -1,4 +1,6 @@
-﻿namespace Microsoft.AzureIntegrationMigration.Runner.Model
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+namespace Microsoft.AzureIntegrationMigration.Runner.Model
 {
     /// <summary>
     /// Defines a base interface for a provider that returns a model instance.
